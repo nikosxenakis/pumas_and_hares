311d6a65412e635a96990ffe74535aada8702233
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2018-10-16 23:47:35 +0100

    main function cleared

--- main.cpp
+++ main.cpp
@@ -1,50 +1,19 @@
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-
-#include "include/puma.hpp"
 #include "include/landscape.hpp"
-#include "include/landscapeParser.hpp"
 #include "include/helpers.hpp"
 #include "include/parser.hpp"
 
-#define RESOURCES_PATH "./resources"
-//#define RESOURCES_PATH "/Users/xenis656/Desktop/PS/pumas_and_hares/pumas_and_hares/resources" //Just for Nikos Xcode
+//#define RESOURCES_PATH "./resources"
+#define RESOURCES_PATH "/Users/xenis656/Desktop/PS/pumas_and_hares/pumas_and_hares/resources" //Just for Nikos Xcode
 
 //write the parameters in a json configuration file
 
-
 int main() {
-    //Puma* puma = new Puma();
-    //LandscapeParser::parseFile(".resources/small.dat");
-    time_t t = time(NULL);
-    srand (t);
 
-/*
-    ifstream landFile;
-
-    landFile.open(string(RESOURCES_PATH) + "/small10x10.dat");
-    
-    vector< vector<int> > tilesVector;
-    
-    if (landFile.is_open()) {
-        tilesVector = Helpers::initTilesVector(landFile);
-    }
-    else {
-        cout << "File is not open" << endl;
-        exit(1);
-    }
-
-    Landscape::init(tilesVector);
-*/
+    Helpers::initRandomGenerator();
 
     parser::parse(string(RESOURCES_PATH) + "/small10x10.dat");
 
     Helpers::simulationLoop();
-
-    cout << (Landscape::getSumDensityNeighbours("Hare", 2, 2)) << endl;
-    
-    cout << Landscape::getNumberOfLandNeighbours(2, 3);
     
     Landscape::destroy();
     
