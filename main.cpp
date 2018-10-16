097ab1a79dd6aecafd3f91c7103085f3d35aeeb4
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2018-10-16 23:48:39 +0100

    minor change added

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include "include/helpers.hpp"
 #include "include/parser.hpp"
 
-//#define RESOURCES_PATH "./resources"
-#define RESOURCES_PATH "/Users/xenis656/Desktop/PS/pumas_and_hares/pumas_and_hares/resources" //Just for Nikos Xcode
+#define RESOURCES_PATH "./resources"
+//#define RESOURCES_PATH "/Users/xenis656/Desktop/PS/pumas_and_hares/pumas_and_hares/resources" //Just for Nikos Xcode
 
 //write the parameters in a json configuration file
 
