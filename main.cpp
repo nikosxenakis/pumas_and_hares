d56799fb533b0d1d427e61734536f901fb3b95ad
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2018-10-17 19:10:52 +0100

    helpers tide up

--- main.cpp
+++ main.cpp
@@ -1,21 +1,12 @@
-#include "include/landscape.hpp"
 #include "include/helpers.hpp"
-#include "include/parser.hpp"
-
-#define RESOURCES_PATH "./resources"
-//#define RESOURCES_PATH "/Users/xenis656/Desktop/PS/pumas_and_hares/pumas_and_hares/resources" //Just for Nikos Xcode
-
-//write the parameters in a json configuration file
 
 int main() {
 
-    Helpers::initRandomGenerator();
-
-    parser::parse(string(RESOURCES_PATH) + "/small10x10.dat");
+    Helpers::init();
 
     Helpers::simulationLoop();
     
-    Landscape::destroy();
+    Helpers::close();
     
     return 0;
 }
--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,8 +1,13 @@
-#include "../include/puma.hpp"
-#include "../include/landscape.hpp"
-#include "../include/landscapeParser.hpp"
 #include "../include/helpers.hpp"
 
+void Helpers::init() {
+    Helpers::initRandomGenerator();
+    parser::parse(string(RESOURCES_PATH) + "/small10x10.dat");
+}
+
+void Helpers::close() {
+    Landscape::destroy();
+}
 
 void Helpers::simulationLoop() {
 
@@ -22,41 +27,3 @@ void Helpers::initRandomGenerator() {
     time_t t = time(NULL);
     srand ((unsigned int)t);
 }
-
-vector< vector<int> > Helpers::initTilesVector(ifstream &landFile) {
-    vector< vector<int> > tilesVector;
-
-     int NY, NX, val;
-     landFile >> NX;
-     if (0 > NX || NX > 2000) {
-         cout << "Number of columns must be between 1 and 2000" << endl;
-         exit(1);
-     }
-     landFile >> NY;
-     if (0 > NY || NY > 2000) {
-                     cout << "Number of rows must be between 1 and 2000" << endl;
-         exit(1);
-     }
-
-     for (int i = 0; i < NX; ++i)
-     {
-         vector<int> tilesLine;
-         for (int j = 0; j < NY; ++j)
-         {
-             landFile >> val;
-
-             if (val != 0 && val != 1) {
-                 cout << "tile must be 1 or 0" << endl;
-                 exit(1);
-             }
-             else {
-                 tilesLine.push_back(val);
-             }
-         }
-         tilesVector.push_back(tilesLine);
-     }
-
-     return tilesVector;
- }
-
-
