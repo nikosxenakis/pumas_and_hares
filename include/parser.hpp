eecf6550e4521b22cac23a591669a2d05cc658c8
Holly Tetlow <s1886165@sms.ed.ac.uk>
2018-10-17 19:43:32 +0100

    Added input functionality for reading in densities

--- include/parser.hpp
+++ include/parser.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <sstream>
 #include <string>
 // inlude json 
 #include "hare.hpp"
@@ -11,14 +12,16 @@
 #include "landscape.hpp"
 #include "pixel.hpp"
 #include "image.hpp"
+#include "inputTile.hpp"
 
 using namespace std;
 
 class parser {
 
 	private:
-
-    		static void parseInput(const string& landFile);
+        static void split(const string& line, vector <string> &vline, char delim);
+        static void errorCheck(vector <string> vtile);
+    	static void parseInput(const string& landFile);
 		static void parseConfig(const string& configFile);
 	public:
 		static void parse(const string& landFileName);
--- src/parser.cpp
+++ src/parser.cpp
@@ -10,14 +10,41 @@ void parser::parse(const string& landFileName) {
 
 }
 
+void parser::split(const string& str, vector<string> &cont, char delim) {
+    stringstream ss(str);
+    string token;
+    cont.clear();
+    while (getline(ss, token, delim)) {
+        cont.push_back(token);
+    }
+}
+
+void parser::errorCheck(vector<string> vTile) {
+    if (stoi(vTile[0])!= 0 && stoi(vTile[0])!= 1) {
+        cout << "Land input must be 0 or 1" << endl;
+        exit(1);
+    }
+    if (stod(vTile[1])<0 || stod(vTile[2]) <0) {
+        cout << "Density input must be positive" << endl;
+        exit(1);
+    }
+
+}
+
 void parser::parseInput(const string& landFileName) {
 
    ifstream landFile;
    landFile.open(landFileName);
-   int NY, NX, val;
+   int NY, NX;
+
+   string inputLine;
+   vector <string> vInputLine;
+   vector <string> vInputTile;
+
    vector< vector<InputTile*> > tilesVector;
    
    InputTile* halo = new InputTile(0, 0.0, 0.0);
+   InputTile* LandTile;
 
    if (landFile.is_open()) {
       landFile >> NX;
@@ -30,32 +57,38 @@ void parser::parseInput(const string& landFileName) {
          cout << "Number of rows must be between 1 and 2000" << endl;
          exit(1);
       }
-      vector<InputTile*> zerosLine (NY + 2);
+
+
+      vector<InputTile*> zerosLine (NX + 2);
       fill (zerosLine.begin(), zerosLine.end(), halo);
       tilesVector.push_back(zerosLine);
-
+      landFile.ignore();
       for (int i = 0; i < NY; ++i) {
 
          vector<InputTile*> tilesLine;
          tilesLine.push_back(halo);
 
-         // read in line
-         // split by space
-         // store as NX strings
-         // split by comma
-          for (int j=0; j<NY;++j) {
-
-             landFile >> val;
-
-             if (val != 0 && val != 1) {
-                 cout << "tile must be 1 or 0" << endl;
-                 exit(1);
-             }
-             else {
-
-                InputTile* LandTile = new InputTile(val);
+         getline(landFile, inputLine);
+         split(inputLine, vInputLine, ' ');
+         
+         if (vInputLine.size()!=NX) {
+            cout << "Error x elements not equal to Landscape size" << endl;
+            exit(1);
+         }
+         
+         for (int j=0; j<NX; j++) {
+                split(vInputLine[j], vInputTile, ',');
+                cout << vInputTile.size() << endl;
+                if (vInputTile.size()==3) {
+                    LandTile = new InputTile(stoi(vInputTile[0]), stod(vInputTile[1]), stod(vInputTile[2]));
+                }
+                else if (vInputTile.size()==1) {
+                    LandTile = new InputTile(stoi(vInputTile[0]));
+                }
+                else {
+                    cout << "Incorrect defintion of input square in input.dat" << endl;
+                }
                 tilesLine.push_back(LandTile);
-             }
           }
           tilesLine.push_back(halo);
           tilesVector.push_back(tilesLine);
