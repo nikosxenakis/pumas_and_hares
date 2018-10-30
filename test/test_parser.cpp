48d75f9a28bf9c319c50e1d104dfb263da31a80e
markxo <mark@tak-media.com>
2018-10-30 20:55:03 +0000

    Parser::parseConfig() tests

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -14,11 +14,43 @@ using namespace std;
 
 TEST_CASE( "ParseInput Test", "[Testing ParseInput]" ){
 
-    GIVEN(""){
-        
-        THEN( "" ){
+    GIVEN( "There are correct config parameters defined in a json file" ){
+
+        string configFileName = string(RESOURCES_PATH) + "/param.json";
+
+        THEN( "Parser sets the correct config parameters to ConfigData, Hare and Puma" ){
+
+            REQUIRE(Hare::getBirthRate() == 1.0);
+            REQUIRE(Hare::getDiffusionRate() == 1.0);
+            REQUIRE(Hare::getPredationRate() == 1.0);
+
+            REQUIRE(Puma::getBirthRate() == 1.0);
+            REQUIRE(Puma::getDiffusionRate() == 1.0);
+            REQUIRE(Puma::getMortalityRate() == 1.0);
+
+            REQUIRE(ConfigData::getCapitalT() == 1.0);
+            REQUIRE(ConfigData::getDeltaT() == 1.0);
+            REQUIRE(Hare::getMortalityRate() == 1.0);
 
         }
     }
 }
 
+// todo: refactor Parser to modularise reading config file, parsing json, setting configData etc
+
+TEST_CASE( "Read json file", "[Reading json file]" ){
+    GIVEN( "A file handle" ){
+        THEN( "The file can be read" ){
+
+        }
+    }
+
+    GIVEN( "A json string" ) {
+        WHEN( "The json can be parsed" ){
+            THEN( "There are all necessary parameter keys in json") {
+
+            }
+        }
+
+    }
+}
