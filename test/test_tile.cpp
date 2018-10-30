489bf197fe473d33498a5563e20c3f5282626e58
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2018-10-30 17:38:13 +0000

    test tile updated

--- test/test_tile.cpp
+++ test/test_tile.cpp
@@ -3,35 +3,61 @@
  * Tests tile class and its functions
  *
  */
-
 #include <iostream>
-#include <fstream>
 #include "catch.hpp"
 #include "../include/tile.hpp"
 #include "../include/landscape.hpp"
 #include "../include/helpers.hpp"
 
 TEST_CASE( "Tile test", "[testing the tiles]"){
-    REQUIRE( 1 == 1 );
-    GIVEN("A landscape of size 3x3"){
 
-       string land_file = "./resources/small3x3.dat";
+    GIVEN("A landscape of size 10x10") {
 
-       Helpers::init(land_file);
-      
-       WHEN(" the Landscape is initilised and one time step occurs" ){ 
-           
-          Landscape::calculate();
-          Landscape::update();
+        string land_file = "./resources/small10x10.dat";
 
+        Helpers::init(land_file);
+        Tile* t1 = Landscape::getTile(3, 3);
+        Tile* t2 = Landscape::getTile(1, 1);
+        
+        WHEN(" the Landscape is initilised" ) { 
+            THEN(" Values of initial land, pumas and hares are as expected ") {
+                REQUIRE( t1->isLand() == Approx(1) );
+                REQUIRE( t1->getOldPumas() == Approx(1.0) );
+                REQUIRE( t1->getOldHares() == Approx(2.0) );
+                REQUIRE( t2->isLand() == Approx(0) );
+                REQUIRE( t2->getOldPumas() == Approx(0) );
+                REQUIRE( t2->getOldHares() == Approx(0) );
+            }
+        }
 
-          THEN(" Values of old hares and pumas are as expexted "){
+        WHEN(" the Landscape is initilised and one time step occurs" ) { 
+            Landscape::calculate();
+            Landscape::update();
 
-               Tile* t = Landscape::getTile(2,2);
-               REQUIRE( t->getOldHares() == Approx(0.696) );
-               REQUIRE( t->getOldPumas() == Approx(0.664) );
+            THEN(" Values of old hares and pumas are as expected ") {
+                REQUIRE( t1->isLand() == Approx(1) );
+                REQUIRE( t1->getOldPumas() == Approx(1.632) );
+                REQUIRE( t1->getOldHares() == Approx(2.432) );
+                REQUIRE( t2->isLand() == Approx(0) );
+                REQUIRE( t2->getOldPumas() == Approx(0) );
+                REQUIRE( t2->getOldHares() == Approx(0) );
+            }
+        }
 
-          }
-       }
+        WHEN(" the Landscape is initilised and the last time step occurs" ) { 
+            for (int t = 2; t <= MAX_STEP; t++) {
+                Landscape::calculate();
+                Landscape::update();
+            }
+
+            THEN(" Values of old hares and pumas are as expected ") {
+                REQUIRE( t1->isLand() == Approx(1) );
+                REQUIRE( t1->getOldPumas() == Approx(2.47594) );
+                REQUIRE( t1->getOldHares() == Approx(10.03977) );
+                REQUIRE( t2->isLand() == Approx(0) );
+                REQUIRE( t2->getOldPumas() == Approx(0) );
+                REQUIRE( t2->getOldHares() == Approx(0) );
+            }
+        }
     }
 }
