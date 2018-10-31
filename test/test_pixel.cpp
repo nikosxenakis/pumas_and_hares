797017df853ee0ab61f81461e5f82010f9c5e41c
Holly Tetlow <s1886165@sms.ed.ac.uk>
2018-10-31 18:26:07 +0000

    Added tests for pixel

--- test/test_pixel.cpp
+++ test/test_pixel.cpp
@@ -8,6 +8,12 @@ TEST_CASE( "Pixel Test", "[Testing pixel's getters and setters]" ){
     GIVEN( " A pixel "){
         Pixel p = Pixel();
         
+        THEN( "The pixel is set to blue "){
+            REQUIRE(p.getRed() == 64);
+            REQUIRE(p.getGreen() == 164);
+            REQUIRE(p.getBlue() == 223);
+        }
+
         WHEN( "We set the pixel's value to an edge case "){
             p.setColour(Colour::hares, 3.0, 0, 0);
 
@@ -15,6 +21,15 @@ TEST_CASE( "Pixel Test", "[Testing pixel's getters and setters]" ){
                 REQUIRE(p.getGreen() == 254);
             }
 
+        }
+        WHEN( "We set the pixel colour") {
+            p.setColour(Colour::pumas, 1.0, 2.0, 2.0);
+
+            THEN( "The pixel colour is set correctly"){
+                REQUIRE(p.getRed() == 254);
+                REQUIRE(p.getGreen() == 127);
+                REQUIRE(p.getBlue() == 127);
+            }
         }
     }
 
