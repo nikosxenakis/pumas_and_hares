7bd7697bcc6c8293045747b0f750bf2c46b990f5
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2018-11-02 16:55:02 +0000

    minor change

--- src/tileData.cpp
+++ src/tileData.cpp
@@ -7,9 +7,12 @@ TileData::TileData(int land, float pumas, float hares) {
 }
 
 TileData::TileData(int land) {
+    this->clear();
     this->land = land;
-    this->pumas = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/5));
-    this->hares = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/5));
+    if(this->land) {
+        this->pumas = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/5));
+        this->hares = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/5));
+    }
 }
 
 void TileData::clear() {
