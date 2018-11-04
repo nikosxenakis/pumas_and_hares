6565fa9081f7218a9fd32cf5779a47dcd38a0b85
nikosxenakis <s1770309@ed.ac.uk>
2018-11-04 19:44:53 +0000

    scripts updated

--- scripts/pumas_and_hares.sh
+++ scripts/pumas_and_hares.sh
@@ -1,4 +1,15 @@
 # make run ./resources/input_files/islands.dat;
-make run ./resources/input_files/islands.dat ./resources/configurations/param.json;
-display ./output/average_density.eps;
-display -sample 200 -delay 1 ./output/Density_*.ppm;
+# make run ./resources/input_files/islands.dat ./resources/configurations/param.json;
+# display ./output/average_density.eps;
+# display -sample 200 -delay 1 ./output/Density_*.ppm;
+
+for input_file in ./resources/input_files/*.dat
+do
+  for config_file in ./resources/configurations/*.json
+  do
+  	echo "make run ${input_file} ${config_file}" >> ./scripts/log.txt;
+    make run $input_file $config_file >> ./scripts/log.txt;
+	# display ./output/average_density.eps;
+	# display -sample 200 -delay 1 ./output/Density_*.ppm;
+  done
+done
