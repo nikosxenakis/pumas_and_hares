f1f83ccb2d0562ed0d2aa17ab9d56c1af313c799
nikosxenakis <s1770309@ed.ac.uk>
2018-11-04 19:24:36 +0000

    display added in scripts

--- scripts/pumas_and_hares.sh
+++ scripts/pumas_and_hares.sh
@@ -1,2 +1,4 @@
 # make run ./resources/input_files/islands.dat;
 make run ./resources/input_files/islands.dat ./resources/configurations/param.json;
+display ./output/average_density.eps;
+display -sample 200 -delay 1 ./output/Density_*.ppm;
