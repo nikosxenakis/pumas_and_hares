7174b421e100a815009e0f7aa756fd73ec7bf211
markxo <mark@tak-media.com>
2018-11-05 14:06:17 +0000

    Helpers::init() test

--- test/src/test_helpers.cpp
+++ test/src/test_helpers.cpp
@@ -7,15 +7,23 @@
 
 #include STR(HEADERS_PATH/configData.hpp)
 #include STR(HEADERS_PATH/helpers.hpp)
+#include STR(HEADERS_PATH/parser.hpp)
 
 using namespace std;
 
 TEST_CASE( "SimulationLoop Test", "[Testing Helpers::simulationLoop()]" ){
 
-    GIVEN( "" ){
+    GIVEN( "configFile and landFile inputs" ){
+        string file = STR(INPUT_FILES_PATH/small3x3.dat);
+        string config_file = STR(CONFIG_PATH/param.json);
 
-        THEN( "" ){
+        WHEN( "configFile and landFile can be parsed and Parser does not throw error" ) {
+            REQUIRE_NOTHROW(Parser::parseConfig(config_file));
+            REQUIRE_NOTHROW(Parser::parseInput(file));
 
+            THEN( "Helpers gets initialised" ) {
+                REQUIRE_NOTHROW(Helpers::init(file, config_file));
+            }
         }
     }
 }
