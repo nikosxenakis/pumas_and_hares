fa6e8f19cc4a69874b49077bed51ce360bc9365d
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2018-11-05 17:10:24 +0000

    helpers test added

--- test/src/test_helpers.cpp
+++ test/src/test_helpers.cpp
@@ -1,29 +1,45 @@
 /* @file helpers_test.cpp
  *
  * @brief Tests helpers class and its functions
- * 
+ *
  * @ingroup Test
  */
 #include "../test_declarations.hpp"
 
-#include STR(HEADERS_PATH/configData.hpp)
 #include STR(HEADERS_PATH/helpers.hpp)
-#include STR(HEADERS_PATH/parser.hpp)
 
 using namespace std;
 
 TEST_CASE( "SimulationLoop Test", "[Testing Helpers::simulationLoop()]" ){
 
     GIVEN( "configFile and landFile inputs" ){
-        string file = STR(INPUT_FILES_PATH/small3x3.dat);
-        string config_file = STR(CONFIG_PATH/param.json);
 
-        WHEN( "configFile and landFile can be parsed and Parser does not throw error" ) {
-            REQUIRE_NOTHROW(Parser::parseConfig(config_file));
-            REQUIRE_NOTHROW(Parser::parseInput(file));
+        WHEN( "configFile and landFile are valid" ) {
+            string inputFile = STR(INPUT_FILES_PATH/small3x3.dat);
+            string config_file = STR(CONFIG_PATH/param.json);
 
-            THEN( "Helpers gets initialised" ) {
-                REQUIRE_NOTHROW(Helpers::init(file, config_file));
+            THEN( "Helpers gets initialised and simulation runs properly" ) {
+                REQUIRE_NOTHROW(Helpers::init(inputFile, config_file));
+                Helpers::simulationLoop();
+                Helpers::close();
+            }
+        }
+
+        WHEN( "inputFile is invalid" ) {
+            string inputFile = STR(INPUT_FILES_PATH/cols_less_1.dat);
+            string config_file = STR(CONFIG_PATH/param.json);
+
+            THEN( "Helper initialization throws exception" ) {
+                REQUIRE_THROWS(Helpers::init(inputFile, config_file));
+            }
+        }
+
+        WHEN( "configFile is invalid" ) {
+            string inputFile = STR(INPUT_FILES_PATH/small3x3.dat);
+            string config_file = STR(CONFIG_PATH/invalid_param.json);
+
+            THEN( "Helper initialization throws exception" ) {
+                REQUIRE_THROWS(Helpers::init(inputFile, config_file));
             }
         }
     }
