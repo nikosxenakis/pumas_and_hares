039b7f338a56576b0cdc0864a954d6e99f9a8221
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2018-11-05 17:35:08 +0000

    test landscape updated

--- test/src/test_landscape.cpp
+++ test/src/test_landscape.cpp
@@ -9,7 +9,6 @@
 #include <fstream>
 
 #include STR(HEADERS_PATH/landscape.hpp)
-#include STR(HEADERS_PATH/tile.hpp)
 #include STR(HEADERS_PATH/helpers.hpp)
 
 using namespace std;
@@ -21,40 +20,42 @@ TEST_CASE( "Landscape Test", "[Landscape is generated and functions used]" ){
        string land_file = STR(INPUT_FILES_PATH/small3x3.dat);
        string config_file = STR(CONFIG_PATH/param.json);
 
-       WHEN(" the Landscape is initilised" ){ 
+       WHEN(" the Landscape is initilised" ){
           Helpers::init(land_file, config_file);
 
-          int max_index = Landscape::getRows() -1; // This works because this particular land
-                                                // file is a square
+          int maxRow = Landscape::getRows() -1;
+          int maxCol = Landscape::getCols() -1;
 
           THEN(" the landscape is an island"){
                // Check edges
-               REQUIRE( Landscape::getTile(0,0)->isLand() == 0 );
-               REQUIRE( Landscape::getTile(max_index,0)->isLand() == 0);
-               REQUIRE( Landscape::getTile(0,max_index)->isLand() == 0);
-               REQUIRE( Landscape::getTile(max_index,max_index)->isLand() == 0);
-    
+               REQUIRE(!Landscape::getTile(0,0)->isLand());
+               REQUIRE(!Landscape::getTile(maxRow,0)->isLand());
+               REQUIRE(!Landscape::getTile(0,maxCol)->isLand());
+               REQUIRE(!Landscape::getTile(maxRow,maxCol)->isLand());
                // Check centre
-               REQUIRE( Landscape::getTile(2,2)->isLand() );
+               REQUIRE(Landscape::getTile(2,2)->isLand());
           }
-          
-     //     THEN(" the density of its neighbours is 0 "){
-     //          REQUIRE( Landscape::getSumDensityNeighbours(Puma::getName(), 1,1) == Approx(0.0) );
-     //          REQUIRE( Landscape::getSumDensityNeighbours(Hare::getName(), 1,1) == Approx(0.0) );
-     //     }
+
           THEN(" It is initialised with a halo"){
                REQUIRE( Landscape::getRows() == 5 );
                REQUIRE( Landscape::getCols() == 5 );
 
           }
+
           THEN(" We can set max pumas and hares "){
               REQUIRE( Landscape::getMaxPumas() == Approx(1.0) );
               REQUIRE( Landscape::getMaxHares() == Approx(1.0) );
-   
+
           }
+
           THEN(" We can calculate the average puma and hare density "){
               REQUIRE( Landscape::getAverageHares() == Approx(1.0/9.0) );
               REQUIRE( Landscape::getAveragePumas() == Approx(1.0/9.0) );
+          }
+
+          THEN(" The simulation runs correctly through the Helpers"){
+              Helpers::simulationLoop();
+              Helpers::close();
           }
        }
     }
