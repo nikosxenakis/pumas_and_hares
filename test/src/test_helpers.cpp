df24a5edf1486404e2a88a2528656c2da5eb1f47
markxo <mark@tak-media.com>
2018-11-05 13:56:16 +0000

    created file for test_helpers.cpp

---
+++ test/src/test_helpers.cpp
@@ -0,0 +1,21 @@
+/* helpers_test.cpp
+ *
+ * Tests helpers class and its functions
+ *
+ */
+#include "../test_declarations.hpp"
+
+#include STR(HEADERS_PATH/configData.hpp)
+#include STR(HEADERS_PATH/helpers.hpp)
+
+using namespace std;
+
+TEST_CASE( "SimulationLoop Test", "[Testing Helpers::simulationLoop()]" ){
+
+    GIVEN( "" ){
+
+        THEN( "" ){
+
+        }
+    }
+}
