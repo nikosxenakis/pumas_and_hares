a825cf1416a1941d9171fdc6ef0764fcd36cfeec
Mark Klaisoongnoen <t.m.klaisoongnoen@sms.ed.ac.uk>
2018-11-13 18:49:20 +0000

    added comments to make_gprof_profile.sh

--- performance_experiments/make_gprof_profile.sh
+++ performance_experiments/make_gprof_profile.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # 1. compile your program with GNU profiler flag "-pg"
-# 2. write gprof profile output to txt file
+# 2. run your newly compiled exectuable - this will create a gmon.out file
+# 3. write gprof profile output to txt file
 gprof ./../bin/pumas_and_hares >> gprof_profile.txt
